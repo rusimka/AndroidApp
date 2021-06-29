1b5ec1f02f30e778f722e064f08b7c5220804e2e
rusimka <rusimkad@gmail.com>
2021-06-30 00:03:47 +0200

    added countdown timer for each question, and when the seconds are less than 10, the coundown color is changing to red

--- app/src/main/java/com/example/androidapp/QuizActivity.java
+++ app/src/main/java/com/example/androidapp/QuizActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -16,11 +17,14 @@ import android.widget.Toast;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class QuizActivity extends AppCompatActivity {
 
 
     public static final String EXTRA_SCORE = "extraScore";
+
+    private static final long COUNTDOWN_IN_MILLIS = 30000;
 
 
     private TextView textViewQuestion;
@@ -34,6 +38,11 @@ public class QuizActivity extends AppCompatActivity {
     private Button buttonConfirmNext;
 
     private ColorStateList textColorDefaulRb;
+    private ColorStateList textColorDefaultCd;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
+
 
     private List<Question> questionList;
     private int questionCounter;
@@ -56,6 +65,8 @@ public class QuizActivity extends AppCompatActivity {
         textViewQuestion = findViewById(R.id.text_view_question);
         textViewScore = findViewById(R.id.text_view_score);
         textViewQuestionCount = findViewById(R.id.text_view_question_count);
+        textViewCountDown = findViewById(R.id.text_view_countdown);
+
         rbGroup = findViewById(R.id.radio_group);
         rb1 = findViewById(R.id.radio_button1);
         rb2 = findViewById(R.id.radio_button2);
@@ -63,6 +74,8 @@ public class QuizActivity extends AppCompatActivity {
         buttonConfirmNext = findViewById(R.id.button_confirm_next);
 
         textColorDefaulRb = rb1.getTextColors();
+        textColorDefaultCd = textViewCountDown.getTextColors();
+
 
 
         QuizDbHelper dbHelper = new QuizDbHelper(this);
@@ -106,13 +119,55 @@ public class QuizActivity extends AppCompatActivity {
             textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
             answered = false;
             buttonConfirmNext.setText("Confirm");
+
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
+            
         } else {
             finishQuiz();
         }
     }
 
+
+
+    private void startCountDown() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis = 0;
+                updateCountDownText();
+                checkAnswer();
+
+            }
+        }.start();
+
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) ((timeLeftInMillis / 1000) / 60);
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes,seconds);
+        textViewCountDown.setText(timeFormatted);
+
+        if (timeLeftInMillis < 10000) {
+            textViewCountDown.setTextColor(Color.RED);
+        } else {
+            textViewCountDown.setTextColor(textColorDefaultCd);
+        }
+
+    }
+
     private void checkAnswer() {
         answered = true;
+
+        countDownTimer.cancel();
+
         RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
         int answerNr = rbGroup.indexOfChild(rbSelected) + 1;
 
@@ -175,4 +230,11 @@ public class QuizActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+    }
 }
